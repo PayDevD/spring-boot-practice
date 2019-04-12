8ac9e6ee543ea695843979735a1e0d4bb50cdbae
PayDevD <mrpaydfordev@gmail.com>
2019-04-12 18:18:28 +0900

    edit typos api

--- src/main/java/org/cnu/realcoding/springbootpractice/api/LoLApiClient.java
+++ src/main/java/org/cnu/realcoding/springbootpractice/api/LoLApiClient.java
@@ -12,10 +12,15 @@ public class LoLApiClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    private String requestUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}api_key={ApiKey}";
+    private String requestUrlByName = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}api_key={ApiKey}";
 
-    public Summoner getSummoner(String SummonerName){
-        return restTemplate.exchange(requestUrl, HttpMethod.GET, null, Summoner.class, SummonerName, "RGAPI-f84556fe-074b-4217-9b64-57822d544d8a")
+    private String requestUrlByEncryptedSummerId = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/{EncryptedSummonerId}api_key={ApiKey}";
+    public Summoner getSummonerByName(String SummonerName){
+        return restTemplate.exchange(requestUrlByName, HttpMethod.GET, null, Summoner.class, SummonerName, "RGAPI-f84556fe-074b-4217-9b64-57822d544d8a")
+                .getBody();
+    }
+    public Summoner getSummonerByEncryptedSummonerId(String encryptedSummonerId){
+        return restTemplate.exchange(requestUrlByEncryptedSummerId, HttpMethod.GET, null, Summoner.class, encryptedSummonerId, "RGAPI-f84556fe-074b-4217-9b64-57822d544d8a")
                 .getBody();
     }
 
