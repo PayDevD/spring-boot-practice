5e64d423545ce0f2a0ecceb446bfbab3ece859e0
PayDevD <mrpaydfordev@gmail.com>
2019-04-15 15:07:01 +0900

    fix ApiClient

--- src/main/java/org/cnu/realcoding/springbootpractice/api/LoLApiClient.java
+++ src/main/java/org/cnu/realcoding/springbootpractice/api/LoLApiClient.java
@@ -1,6 +1,7 @@
 package org.cnu.realcoding.springbootpractice.api;
 
 
+import org.cnu.realcoding.springbootpractice.domain.LeaguePosition;
 import org.cnu.realcoding.springbootpractice.domain.Summoner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -12,15 +13,15 @@ public class LoLApiClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    private String requestUrlByName = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}api_key={ApiKey}";
+    private String requestUrlByName = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}?api_key={ApiKey}";
 
-    private String requestUrlByEncryptedSummerId = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/{EncryptedSummonerId}api_key={ApiKey}";
+    private String requestUrlByEncryptedSummerId = "https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/{encryptedSummonerId}?api_key={ApiKey}";
     public Summoner getSummonerByName(String SummonerName){
-        return restTemplate.exchange(requestUrlByName, HttpMethod.GET, null, Summoner.class, SummonerName, "RGAPI-f84556fe-074b-4217-9b64-57822d544d8a")
+        return restTemplate.exchange(requestUrlByName, HttpMethod.GET, null, Summoner.class, SummonerName, "RGAPI-1b9188d0-d32e-4716-8dff-1cceff282361")
                 .getBody();
     }
-    public Summoner getSummonerByEncryptedSummonerId(String encryptedSummonerId){
-        return restTemplate.exchange(requestUrlByEncryptedSummerId, HttpMethod.GET, null, Summoner.class, encryptedSummonerId, "RGAPI-f84556fe-074b-4217-9b64-57822d544d8a")
+    public LeaguePosition[] getLeaguePositionByEncryptedSummonerId(String encryptedSummonerId){
+        return restTemplate.exchange(requestUrlByEncryptedSummerId, HttpMethod.GET, null, LeaguePosition[].class, encryptedSummonerId, "RGAPI-1b9188d0-d32e-4716-8dff-1cceff282361")
                 .getBody();
     }
 
